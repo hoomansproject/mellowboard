8c93fe9eb818c68202488b5f16caedf17f768f42
M Viraj Prabhu <virajprabhup@gmail.com>
2025-07-20 20:18:26 +0530

    feat: added-cron-route

--- src/app/api/run-cron-job.ts
+++ src/app/api/run-cron-job.ts
@@ -1,14 +1,15 @@
 // src/pages/api/run-cron-job.ts
 import type { NextApiRequest, NextApiResponse } from "next";
 import { runCronJob } from "@/server/tasks/cron";
+import { env } from "@/env";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  if (req.headers.authorization !== `Bearer ${process.env.CRON_SECRET}`) {
+  if (req.headers.authorization !== `Bearer ${env.CRON_SECRET}`) {
     return res.status(401).end("Unauthorized");
   }
   await runCronJob();
-  res.status(200).json({ success: true });
+  res.status(200).json({ success: true, message: "Cron job executed" });
 }
