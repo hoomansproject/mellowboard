48a172d2ee8046dc9d2e4c712021f807c4a09119
M Viraj Prabhu <virajprabhup@gmail.com>
2025-07-21 00:19:18 +0530

    fix: fixed-prerender

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import Leaderboard from "@/app/_components/leaderboard";
 import { api, HydrateClient } from "@/trpc/server";
 
 export default async function Home() {
-  const dehydratedState = await api.leaderboard.getLeaderboard.prefetch();
+  //  await api.leaderboard.getLeaderboard.prefetch();
   return (
-    <HydrateClient state={dehydratedState}>
+    <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center text-black">
         <Leaderboard />
       </main>
