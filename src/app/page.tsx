6157344ca4230e9526e7b5bed6902f1539c790a9
M Viraj Prabhu <virajprabhup@gmail.com>
2025-07-21 00:15:01 +0530

    fix: fixed-prerender

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Leaderboard from "@/app/_components/leaderboard";
 import { api, HydrateClient } from "@/trpc/server";
 
 export default async function Home() {
-  void api.post.getLatest.prefetch();
   void api.leaderboard.getLeaderboard.prefetch();
 
   return (
