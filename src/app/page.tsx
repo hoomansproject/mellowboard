3b146f94a28816437549d0a83b91ec4d58a05d4f
M Viraj Prabhu <virajprabhup@gmail.com>
2025-07-21 00:18:28 +0530

    fix: check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,9 @@ import Leaderboard from "@/app/_components/leaderboard";
 import { api, HydrateClient } from "@/trpc/server";
 
 export default async function Home() {
-  void api.leaderboard.getLeaderboard.prefetch();
-
+  const dehydratedState = await api.leaderboard.getLeaderboard.prefetch();
   return (
-    <HydrateClient>
+    <HydrateClient state={dehydratedState}>
       <main className="flex min-h-screen flex-col items-center justify-center text-black">
         <Leaderboard />
       </main>
