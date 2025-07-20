1da5bc49b255b792cdd287c35344925e818c050f
M Viraj Prabhu <virajprabhup@gmail.com>
2025-07-20 20:32:08 +0530

    feat(tasks): added-cron-task

--- src/server/tasks/cron.ts
+++ src/server/tasks/cron.ts
@@ -1,3 +1,46 @@
+import { google } from "googleapis";
+import { env } from "@/env";
+import {
+  generateCronLogs,
+  getOrInsertUserIds,
+  parseDates,
+  parseUsernames,
+  updateLogs,
+} from "../utils/log-helpers";
+
 export const runCronJob = async () => {
-  console.log("running cron job");
+  const auth = new google.auth.GoogleAuth({
+    credentials: {
+      client_email: env.GOOGLE_CLIENT_EMAIL,
+      private_key: env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    },
+    scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
+  });
+
+  const sheets = google.sheets({ version: "v4", auth });
+  const res = await sheets.spreadsheets.get({
+    spreadsheetId: env.SHEET_ID,
+    includeGridData: true,
+    ranges: ["Commit Box!A1:Z100"],
+  });
+
+  const rowData = res.data.sheets?.[0]?.data?.[0]?.rowData ?? [];
+
+  const usernames = parseUsernames(rowData);
+  const dates = parseDates(rowData);
+  const userIds = await getOrInsertUserIds(usernames);
+
+  const logsToInsert = await generateCronLogs(
+    rowData,
+    usernames,
+    dates,
+    userIds,
+  );
+
+  if (logsToInsert.length > 0) {
+    await updateLogs(logsToInsert);
+    console.log(`✅ Inserted ${logsToInsert.length} logs.`);
+  } else {
+    console.log("ℹ️ No logs to insert.");
+  }
 };
