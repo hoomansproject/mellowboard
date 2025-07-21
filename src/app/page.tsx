228108a5c3e9e1e2f455a64d436a3785e5aebbad
M Viraj Prabhu <virajprabhup@gmail.com>
2025-07-21 16:04:16 +0530

    fix: removed-prefetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 import Leaderboard from "@/app/_components/leaderboard";
-import { api, HydrateClient } from "@/trpc/server";
+import { HydrateClient } from "@/trpc/server";
 
 export default async function Home() {
-  await api.leaderboard.getLeaderboard.prefetch();
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center text-black">
