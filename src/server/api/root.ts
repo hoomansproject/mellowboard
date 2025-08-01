9f8a92269edd116b986e0d0c0b1dc35b14537e6c
M Viraj Prabhu <virajprabhup@gmail.com>
2025-08-01 22:59:06 +0530

    fix: removed-posts

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { postRouter } from "@/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
 import { leaderboardRouter } from "@/server/api/routers/leaderboard";
 
@@ -8,7 +7,6 @@ import { leaderboardRouter } from "@/server/api/routers/leaderboard";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   leaderboard: leaderboardRouter,
 });
 
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -2,7 +2,7 @@
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
 import { sql } from "drizzle-orm";
-import { index, unique, pgTable } from "drizzle-orm/pg-core";
+import { unique, pgTable } from "drizzle-orm/pg-core";
 
 import { uuid, varchar, integer, timestamp } from "drizzle-orm/pg-core";
 import { logTypePg, logStatusPg } from "./enums/log";
@@ -13,20 +13,6 @@ import { logTypePg, logStatusPg } from "./enums/log";
  *
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
-
-export const posts = pgTable(
-  "post",
-  (d) => ({
-    id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
-    name: d.varchar({ length: 256 }),
-    createdAt: d
-      .timestamp({ withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
-  }),
-  (t) => [index("name_idx").on(t.name)],
-);
 
 //mellowboard tables
 
